a9524ac34e64e65c1c2bb6408ee5bd6fdba558c7
= <=>
2015-11-26 19:48:47 +0600

    #3  Создал action и view на создание Course. Он срабатывает по пути "/courses/new"

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,25 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+
+      redirect_to @course
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name)
+  end
+
 end
