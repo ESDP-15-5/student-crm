de06bb797289c3d184f57354b05f6d8f2b61f3fe
Reedlee <write.to.lee.igor@gmail.com>
2015-12-04 23:43:47 +0600

    #20 добавил проверку в Course_element и отображение ошибок в форму
    Проверяемое поле :course_id, :theme, :element_type на наличие значений перед сохранением

--- app/models/course_element.rb
+++ app/models/course_element.rb
@@ -1,14 +1,19 @@
 class CourseElement < ActiveRecord::Base
-  acts_as_paranoid
+  ELEMENT_TYPES = ['Лекция','Вебинар','Лабораторная','Контрольная']
 
+  acts_as_paranoid
   audited
+
   belongs_to :course
 
   has_many :course_element_files
   has_many :course_element_materials
   has_many :periods
 
-  ELEMENT_TYPES = ['Лекция','Вебинар','Лабораторная','Контрольная']
+  validates :course_id, presence: true
+  validates :theme, presence: true
+  validates :element_type, presence: true,  inclusion: {in: ELEMENT_TYPES}
+
 
   include RankedModel
   ranks :row_order, :with_same => :course_id
