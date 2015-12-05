50edf60b32d12d00b7ae9e83fa9f1520a996327d
Farid <farid.babazov@gmail.com>
2015-12-05 14:42:46 +0600

    #19 Реализовал Handler элементов курса.

--- app/assets/javascripts/update_course_elements.js
+++ app/assets/javascripts/update_course_elements.js
@@ -4,19 +4,36 @@ $(window).on("page:change", function() {
     var $sortable = $('#sortable');
     if ($sortable.length > 0) {
         table_width = $sortable.width();
-        cells = $('.table').find('tr')[0].cells.length;
-        desired_width = table_width / cells + 'px';
-        $('.table td').css('width', desired_width);
+        var $heading = $sortable.find('tr').first();
+        var widths = {};
+
+        var ths = $heading.find('th');
+        for (var i = 0; i < ths.length; i++) {
+            widths[i] = $(ths[i]).width();
+        }
+
+        var rebuildPositions = function() {
+            $sortable.find('[data-display="position"]').each(function(index) {
+                $(this).html(index + 1);
+            });
+        };
+
         $sortable.sortable({
             axis: 'y',
             items: '.item',
             cursor: 'move',
+            handle: '.elements-table-handle',
             sort: function(e, ui) {
                 ui.item.addClass('active-item-shadow');
+                ui.item.find('td').each(function(index) {
+                    $(this).width(widths[index]);
+                });
+                ui.placeholder.height(ui.item.height());
             },
             stop: function(e, ui) {
                 ui.item.removeClass('active-item-shadow');
                 ui.item.children('td').effect('highlight', {}, 1000);
+                rebuildPositions();
             },
             update: function(e, ui) {
                 var item_id, position, url;
