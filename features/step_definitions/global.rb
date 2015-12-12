aaa9033e1bc992799f6489b2564c26c18f612740
Reedlee <write.to.lee.igor@gmail.com>
2015-12-12 17:36:27 +0600

    #25 добавил степы для прохождения теста по CRUD сценарию для course_element

--- features/step_definitions/global.rb
+++ features/step_definitions/global.rb
@@ -76,3 +76,60 @@ When(/^меня редиректит на страницу с авторизац
   expect(page).to have_content('Войти')
   find_button('Войти').click
 end
+
+When(/^пользователь заходит в курс "([^"]*)"$/) do |course_name|
+  # find(course_name).click
+  click_link(course_name)
+end
+
+When(/^он видит список элементов курса "([^"]*)"$/) do |course_name|
+  expect(page).to have_content("Элементы курса #{course_name}")
+  expect(page).to have_css('table')
+end
+
+When(/^пользователь нажимает на "([^"]*)"$/) do |button_name|
+  click_link(button_name)
+end
+
+
+When(/^вводит в название элемента курса "([^"]*)"$/) do |theme|
+  within('#new_course_element') do
+    fill_in 'course_element[theme]', :with => theme
+  end
+end
+
+When(/^выбирает тип элемента курса "([^"]*)"$/) do |type_element|
+  within('#new_course_element') do
+    select(type_element, :from => 'course_element[element_type]')
+  end
+end
+
+When(/^пользователь нажимает на кнопку "([^"]*)"$/) do |button_name|
+  find_button(button_name).click
+end
+
+When(/^"([^"]*)" появляется в списке элементов$/) do |course_element|
+  expect(page).to have_content(course_element)
+  expect(page).to have_selector('table')
+  find_link('Выйти', :visible => :all).visible?
+end
+
+
+When(/^пользователь удаляет элемент курса с названием "([^"]*)"$/) do |element_course|
+  element = "//td//*[contains(text(), '" + element_course + "')]/ancestor::tr//*[contains(text(), 'Удалить')]"
+  find(:xpath, element).click
+  page.driver.browser.switch_to.alert.accept
+end
+
+When(/^элемент курса "([^"]*)" пропадет из списка элементов$/) do |element_course|
+  page.should have_no_content(element_course)
+end
+
+When(/^пользователь редактирует элемент курса  с названием "([^"]*)" на "([^"]*)"$/) do |course_element_name_old, course_element_new|
+  element = "//td//*[contains(text(), '" + course_element_name_old + "')]/ancestor::tr//*[contains(text(), 'Обновить')]"
+  find(:xpath, element).click
+  within('.edit_course_element') do
+    fill_in 'course_element[theme]', :with => course_element_new
+  end
+  click_button('Обновить элемент курса')
+end
