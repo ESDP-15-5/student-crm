ea061585366a730e225cf65de0f79af31750cef7
Reedlee <write.to.lee.igor@gmail.com>
2015-12-12 17:34:07 +0600

    #25 добавил тестовые фикстуры для course_element

--- db/test_seeds.rb
+++ db/test_seeds.rb
@@ -1,6 +1,16 @@
 # ActiveRecord::Migration.drop_table(:users)
 # ActiveRecord::Migration.drop_table(:courses)
+element_type = CourseElement::ELEMENT_TYPES
+
 users = User.create!([{email: 'test@mail.ru', password: '12345678'},{email: 'a@a.a', password: '12345678'}])
 courses=[]
 courses.push Course.create(name: "CourseForDelete", starts_at: nil, ends_at: nil)
 courses.push Course.create(name: "CourseForEdit", starts_at: nil, ends_at: nil)
+courses.push Course.create(name: "HTML", starts_at: nil, ends_at: nil)
+
+CourseElement.create(course: courses[2],
+                     theme: "Введение",
+                     element_type: element_type.sample)
+CourseElement.create(course: courses[2],
+                     theme: "Начало работы",
+                     element_type: element_type.sample)
