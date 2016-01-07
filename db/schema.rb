be33e1f36d8d94fbb3b026e73c1a608f233c1efa
reedlee <reedlee3@yandex.ru>
2016-01-07 20:28:58 +0600

    #36 ПРоизвел чистку после слияния веток
    Удалил ненужные css
    Исправил ошибку в ссылке на шаблон(breadcrums navbar) в файле layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151224154540) do
+ActiveRecord::Schema.define(version: 20151222133816) do
 
   create_table "attendances", force: :cascade do |t|
     t.integer  "student_id"
@@ -147,6 +147,15 @@ ActiveRecord::Schema.define(version: 20151224154540) do
   add_index "periods", ["course_element_id"], name: "index_periods_on_course_element_id"
   add_index "periods", ["deleted_at"], name: "index_periods_on_deleted_at"
 
+<<<<<<< HEAD
+  create_table "sms_service_accounts", force: :cascade do |t|
+    t.string   "login"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+=======
   create_table "roles", force: :cascade do |t|
     t.string   "name"
     t.integer  "resource_id"
@@ -158,13 +167,7 @@ ActiveRecord::Schema.define(version: 20151224154540) do
   add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
   add_index "roles", ["name"], name: "index_roles_on_name"
 
-  create_table "sms_service_accounts", force: :cascade do |t|
-    t.string   "login"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
+>>>>>>> origin/users
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.string   "phone"
@@ -174,16 +177,18 @@ ActiveRecord::Schema.define(version: 20151224154540) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",               default: "", null: false
-    t.string   "encrypted_password",  default: "", null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",       default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                       null: false
-    t.datetime "updated_at",                       null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "name"
     t.string   "surname"
     t.string   "middlename"
@@ -200,6 +205,7 @@ ActiveRecord::Schema.define(version: 20151224154540) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
