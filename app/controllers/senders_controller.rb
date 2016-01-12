1139148d73a19f7403dbce2e15d94a502466e654
Farid <farid.babazov@gmail.com>
2016-01-12 17:58:21 +0600

    #45 Настройка sender и вьюшки для sender

--- app/controllers/senders_controller.rb
+++ app/controllers/senders_controller.rb
@@ -1,2 +1,36 @@
 class SendersController < ApplicationController
+  def get_initial_crums()
+    {
+        "Учетные записи"=> sms_service_accounts_path
+    }
+  end
+  def new
+    @sender = Sender.new
+
+    hash_crums = {
+        "Новый отравитель" => {}
+    }
+    @bread_crums = add_bread_crums(hash_crums)
+
+    # add_breadcrumb 'Учетные записи', :sms_service_accounts_url
+    # add_breadcrumb 'Новый отравитель', :new_sender_url
+  end
+
+  def create
+    @sender = Sender.new(sender_params)
+    if @sender.save
+      redirect_to sms_service_accounts_url
+      flash[:success] = 'Отправитель успешно создан'
+    else
+      flash[:danger] = 'Вы ввели некорректные данные, проверьте и попробуйте снова'
+      render 'new'
+    end
+  end
+
+  private
+
+  def sender_params
+    params.require(:sender).permit(:name, :sms_service_account_id)
+  end
+
 end
