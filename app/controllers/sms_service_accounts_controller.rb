b60a57ca8e039837717b6147da4188730f7ad09f
Farid <farid.babazov@gmail.com>
2016-01-12 17:43:36 +0600

    #45 Настройки связей между таблицами

--- app/controllers/sms_service_accounts_controller.rb
+++ app/controllers/sms_service_accounts_controller.rb
@@ -1,2 +1,78 @@
 class SmsServiceAccountsController < ApplicationController
+  before_action :set_sms_service_account, only: [:show, :edit, :update, :destroy]
+
+  def get_initial_crums()
+    {
+        "Учетные записи"=> sms_service_accounts_path
+    }
+  end
+
+  def index
+    @sms_service_accounts = SmsServiceAccount.all
+    @senders = Sender.all
+  end
+
+  def show
+
+    hash_crums = {
+        "#{@sms_service_account.login}"=> {}
+    }
+    @bread_crums = add_bread_crums(hash_crums)
+  end
+
+  def new
+    @sms_service_account = SmsServiceAccount.new
+    @sms_service_account.senders.build
+    hash_crums = {
+        "Новая учетная запись" => {}
+    }
+    @bread_crums = add_bread_crums(hash_crums)
+
+  end
+
+  def create
+    @sms_service_account = SmsServiceAccount.new(sms_service_account_params)
+    if @sms_service_account.save
+      redirect_to sms_service_accounts_url
+      flash[:success] = 'Учетная запись успешно создана'
+    else
+      flash[:danger] = 'Вы ввели некорректные данные, проверьте и попробуйте снова'
+      render 'new'
+    end
+  end
+
+  def edit
+    @sms_service_account.senders.build
+    hash_crums = {
+        "Редактирование учетной записи -  #{@sms_service_account.login}" => {}
+    }
+
+    @bread_crums = add_bread_crums(hash_crums)
+  end
+
+  def update
+    if @sms_service_account.update(sms_service_account_params)
+      flash[:success] = 'Учетная запись успешно обновлена'
+      redirect_to sms_service_accounts_url
+    else
+      flash[:danger] = 'Вы ввели некорректные данные, проверьте и попробуйте снова'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @sms_service_account.destroy
+    flash[:success] = 'Учетная запись успешно удалена'
+    redirect_to sms_service_accounts_url
+  end
+
+  private
+
+  def set_sms_service_account
+    @sms_service_account = SmsServiceAccount.find(params[:id])
+  end
+
+  def sms_service_account_params
+    params.require(:sms_service_account).permit(:login, :password, senders_attributes: [:id, :name, :sms_service_account])
+  end
 end
--- app/models/sms_delivery.rb
+++ app/models/sms_delivery.rb
@@ -1,2 +1,76 @@
 class SmsDelivery < ActiveRecord::Base
+  belongs_to :contact_list
+  belongs_to :sender
+
+  before_validation :fill_title
+
+  validates :title, presence: true, length: { maximum: 70 }
+  validates :content, presence: true, length: { maximum: 800 }
+  validates :contact_list, presence: true
+  validates :sender, presence: true
+
+  def send_message
+    url = '/api/message'
+    @response = set_url(url, build_message)
+    update_attribute(:status, true)
+  end
+
+  def build_message
+    Nokogiri::XML::Builder.new do |xml|
+      xml.message {
+        xml.login(sender.sms_service_account.login)
+        xml.pwd(sender.sms_service_account.password)
+        xml.id(id.to_s + 'importecsdm')
+        xml.sender(sender.name)
+        xml.text_ content
+        xml.phones {
+
+          if contact_list.custom_lists.any?
+            contact_list.custom_lists.each do |phone|
+              xml.phone(phone.phone)
+            end
+          else
+            contact_list.users.each do |student|
+              xml.phone(student.contact.phone)
+            end
+          end
+
+        }
+      }
+    end
+  end
+
+  def build_report
+    Nokogiri::XML::Builder.new do |xml|
+      xml.dr {
+        xml.login('aisma')
+        xml.pwd('kiminitodoke')
+        xml.id(id.to_s + 'Mesp1502')
+      }
+    end
+  end
+
+  def parse_report(xml)
+    xml_doc = Nokogiri::XML(xml.body)
+    xml_doc.remove_namespaces!
+    doc = xml_doc.xpath('//phone')
+    hash = {}
+    doc.each do |phone|
+      hash[phone.xpath('number').text] = phone.xpath('report').text
+    end
+    hash
+  end
+
+  private
+
+  def set_url (url, message)
+    http = Net::HTTP.new('smspro.nikita.kg')
+    http.post(url, message.to_xml)
+  end
+
+  def fill_title
+    if self.title.blank?
+      self.title = self.content[0, 69]
+    end
+  end
 end
