fd392efcd893885e2619fc9810ac0342dc1cb443
reedlee <reedlee3@yandex.ru>
2016-01-28 21:02:51 +0600

    #57 Удалил дублирование private в periods_controller и неиспользуемы код с цветом группы в periods index.html

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -94,7 +94,6 @@ def get_initial_crums()
   def period_params
     params.require(:period).permit(:group_id,:course_element_id,:title, :commence_datetime)
   end
-  private
   # Use callbacks to share common setup or constraints between actions.
   def set_event
     @period = Period.find(params[:id])
